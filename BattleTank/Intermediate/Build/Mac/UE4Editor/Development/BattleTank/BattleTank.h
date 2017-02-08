a24b1f204b64120c53fea3e6a298c0fc165349fb
Jose F Florez <josefflo@gmail.com>
2017-02-08 10:09:34 -0600

    Creating the project

---
+++ BattleTank/Intermediate/Build/Mac/UE4Editor/Development/BattleTank/BattleTank.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_BATTLETANK_H__
+#define __AUTO_BATTLETANK_H__
+//Last Write: 2/8/2017 3:59:17 PM +00:00
+#include "/Users/florezarango/Documents/Development/Ureal/Section4/04_BattleTank/BattleTank/Source/BattleTank/BattleTank.h"
+#endif//__AUTO_BATTLETANK_H__
